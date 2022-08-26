a55a30993a9e60fc6c80e1e9466351c768132b7d
nettojorge <vasconcelosnetto@gmail.com>
2022-08-26 06:16:05 -0300

    Modificado projeto-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,7 @@
-let name = "Jorg Netto";
-let email = "vasconcelosnetto@gmail.com";
-let profissao = "Geógrafo";
-let idade = "37";
-let lugarsonho = "Austrália";
+let name
+let email
+let profissao
+let idade
+let lugarsonho
 
 
