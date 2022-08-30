5f0ecf8d2fd6cd7128e992cd4fff930c4f362eb8
nettojorge <vasconcelosnetto@gmail.com>
2022-08-30 07:39:05 -0300

    Modificado projeto-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,7 @@
-let name = "Jorg Netto"
-let email = "vasconcelosnetto@gmail.com"
-let profissao = "Geógrafo"
-let idade = "37"
-let lugarsonho = "Austrália"
+let nome = "Jorge Netto\n"
+let email = "vasconcelosnetto@gmail.com\n"
+let profissao = "Geógrafo\n"
+let idade = "37\n"
+let lugarsonho = "Austrália\n"
+
+console.log ('Nome: ', nome, 'E-mail: ', email, 'Profissão: ', profissao, 'Idade: ', idade, 'Lugar dos sonhos: ', lugarsonho);
