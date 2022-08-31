5feca8846f9c4f1728a072455c83989ee48c3372
nettojorge <vasconcelosnetto@gmail.com>
2022-08-31 08:15:42 -0300

    Pratica-3 - Ímpares

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,15 @@
+//Ímpares
+
+// Valores de referência
+let n = 16
+let i = 1
+
+console.log("Valores ímpares de 1 até " + n)
+
+while (i <= n) {
+    if ((i % 2) != 0 ) {
+        console.log(i)
+    }    
+    
+    i++
+}
