9dd595967aba6da46a5aeca7e8c00712f73e3b7f
nettojorge <vasconcelosnetto@gmail.com>
2022-08-31 08:05:01 -0300

    Pratica-2 - Tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,13 @@
+//Tabuada
+
+// Valores de referência
+let numTab = 2
+let n = 10
+let i = 1
+
+console.log("Tabuaba de multiplicação do " + numTab)
+
+while (i <= n) {
+    console.log(numTab + " x " + i + " = " + (numTab * i));
+    i++
+}
