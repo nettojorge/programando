092fae28ab6783a1fcd6cd6af6856b6af226dff4
nettojorge <vasconcelosnetto@gmail.com>
2022-08-30 21:19:36 -0300

    Modificado projeto-2.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,7 @@
-let nome = "Jorge Netto\n"
-let email = "vasconcelosnetto@gmail.com\n"
-let profissao = "Geógrafo\n"
-let idade = "37\n"
-let lugarsonho = "Austrália\n"
+let nome = "Jorge Netto"
+let email = "vasconcelosnetto@gmail.com"
+let profissao = "Geógrafo"
+let idade = "37"
+let lugarsonho = "Austrália"
 
-console.log ('Nome: ', nome, 'E-mail: ', email, 'Profissão: ', profissao, 'Idade: ', idade, 'Lugar dos sonhos: ', lugarsonho);
+console.log ("Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugarsonho + ".\n Confirma pra gente alguns dados?\n A sua idade é " + idade + ", você é "+ profissao + " e o seu email é " + email + "? \n Ficamos no aguardo. Muito obrigado e boa viagem! :D")
